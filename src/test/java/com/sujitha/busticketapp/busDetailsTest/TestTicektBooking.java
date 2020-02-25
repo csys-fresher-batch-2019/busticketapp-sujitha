faa5321baa756418726ea6a5997732f774f03e77
Sujitha S <sujitha.subburaj@global.chainsys.com>
2020-02-25 11:04:56 +0530

    updated

--- src/test/java/com/sujitha/busticketapp/busDetailsTest/TestTicektBooking.java
+++ src/test/java/com/sujitha/busticketapp/busDetailsTest/TestTicektBooking.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.jws.soap.SOAPBinding;
+
 
 import com.sujitha.busticketapp.DbException;
 import com.sujitha.busticketapp.dao.impl.BusListDAOImpl;
